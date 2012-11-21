492b613374d7ea205662d7846f22b5dd5b83c246
Victor Zagorski <victor@zagorski.ru>
2012-11-21 15:58:32 +0800

    Added posts view

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,16 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import DetailView, ListView
+from blog.models import Post
 
-urlpatterns = patterns('blog.views'
-    url(r'^(?:page\/(?P<page\d+))?$', 'index'),
-    url(r'^posts\/(?P<post_id\d+)[^\/]*?$',  'show')
+urlpatterns = patterns(''
+    url(r'^(?:page\/(?P<page\d+))?$',
+      ListView.as_view(
+        queryset = Post.objects.order_by('-pub_date')[:5],
+        context_object_name='posts'
+        template_name='posts/index.html')),
+
+    url(r'^posts\/(?P<post_id\d+)[^\/]*?$',
+      ListView.as_view(
+        model=Post,
+        template_name='posts/show.html'))
 )
